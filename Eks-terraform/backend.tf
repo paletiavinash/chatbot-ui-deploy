9b7f3ea02ea1d3716e030177fbf996dfda0f708b
Paleti Avinash <avinash@Paletis-MacBook-Pro.local>
2024-03-13 13:13:50 +0530

    update s3 bucket

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "sumanthbro" # Replace with your actual S3 bucket name
+    bucket = "chatbotui-legacydeploys3" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
